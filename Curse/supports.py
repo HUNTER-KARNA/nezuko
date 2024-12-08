8c8de617081290038d2f70f8c23519e7dc5f0b4d
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-12-08 14:49:54 +0530

    Update supports.py

--- Curse/supports.py
+++ Curse/supports.py
@@ -1,27 +1,30 @@
 from Curse import DEV_USERS, OWNER_ID, SUDO_USERS, WHITELIST_USERS
 from Curse.database.support_db import SUPPORTS
 
-
 async def load_support_users():
     support = SUPPORTS()
-    for i in DEV_USERS:
-        support.insert_support_user(int(i),"dev")
-    for i in SUDO_USERS:
-        support.insert_support_user(int(i),"sudo")
-    for i in WHITELIST_USERS:
-        support.insert_support_user(int(i),"whitelist")
+
+    def safe_insert(user_list, user_type):
+        for i in user_list:
+            try:
+                if i:
+                    support.insert_support_user(int(i), user_type)
+            except ValueError:
+                pass
+
+    safe_insert(DEV_USERS, "dev")
+    safe_insert(SUDO_USERS, "sudo")
+    safe_insert(WHITELIST_USERS, "whitelist")
+
     return
 
-def get_support_staff(want = "all"):
-    """
-    dev, sudo, whitelist, dev_level, sudo_level, all
-    """
+def get_support_staff(want="all"):
     support = SUPPORTS()
     devs = support.get_particular_support("dev")
     sudo = support.get_particular_support("sudo")
     whitelist = support.get_particular_support("whitelist")
 
-    if want in ["dev","dev_level"]:
+    if want in ["dev", "dev_level"]:
         wanted = devs
     elif want == "sudo":
         wanted = sudo
