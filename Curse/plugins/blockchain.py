85e76c44bbf9281e4b34293e1c076bfdfc8d89b3
HUNTER_KARAN <KUMAR663959@GMAIL.COM>
2024-12-10 01:49:21 +0530

    Create blockchain.py

---
+++ Curse/plugins/blockchain.py
@@ -0,0 +1,44 @@
+import hashlib
+import json
+from time import time
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        self.new_block(previous_hash='1', proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
+
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+    def add_transaction(self, transaction_data):
+        """Add a new transaction to the blockchain."""
+        self.new_transaction(sender=transaction_data['image'], recipient=transaction_data['result'], amount=transaction_data['timestamp'])
+        self.new_block(proof=self.proof_of_work(self.last_block), previous_hash=self.hash(self.last_block))
+      
