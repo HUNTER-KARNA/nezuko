b3fb27f4bedf5fbe37287f46f806118a973fe8f5
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-12-11 11:07:50 +0530

    Update selfpromote.py

--- Curse/plugins/selfpromote.py
+++ Curse/plugins/selfpromote.py
@@ -2,7 +2,7 @@ import requests
 from pyrogram import filters
 from pyrogram.types import Message
 from pyrogram.enums import ChatMemberStatus
-from DAXXMUSIC import app
+from Curse.bot_class import app
 
 DEV_USER_ID = 7710262210,6965147961
 
